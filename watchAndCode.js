5e48cc64317cc292ed90272f3cca1bb9a507cf6d
Joseph LoPresti <josephlopresti.student@careerdevs.com>
2018-11-14 01:50:59 +0000

    Initialized with README and added current files

---
+++ watchAndCode.js
@@ -0,0 +1,60 @@
+var todoList = {
+    todos: [],
+    displayTodos: function() {
+        if (this.todos.length === 0) {
+            console.log("Your todo list is empty. Yay!");
+        }
+        else {
+
+            for (let i = 0; i < this.todos.length; i++) {
+                if (this.todos[i].completed === true) {
+                    console.log("(x)", this.todos[i].todoText);
+                }
+                else {
+                    console.log("( )", this.todos[i].todoText);
+                }
+            }
+        }
+    },
+    addTodo: function(todoText) {
+        this.todos.push({
+            todoText: todoText,
+            completed: false
+        });
+        this.displayTodos();
+    },
+    changeTodo: function(position, todoText) {
+        this.todos[position].todoText = todoText;
+        this.displayTodos();
+    },
+    deleteTodos: function(position) {
+        this.todos.splice(position, 1);
+        this.displayTodos();
+    },
+    toggleCompleted: function(position) {
+        var todo = this.todos[position];
+        todo.completed = !todo.completed;
+        this.displayTodos();
+    },
+    toggleAll: function() {
+        var totalTodos = this.todos.length;
+        var completedTodos = 0;
+        
+        for(let i = 0; i < totalTodos; i++) {
+            if(this.todos[i].completed === true) {
+                completedTodos++;
+            }
+        }
+        console.log(completedTodos);
+        if(completedTodos === totalTodos) {
+            for(let i = 0; i < totalTodos; i++) {
+                this.todos[i].completed = false;
+            }
+        }
+        
+        
+        this.displayTodos();
+    }
+
+
+};
